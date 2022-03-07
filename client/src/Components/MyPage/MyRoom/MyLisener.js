706c31b11d26c934a6116575846478976b2a2e2f
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-03-07 16:11:30 +0900

    MyArtist or MyLisener page Router or front write

--- client/src/Components/MyPage/MyRoom/MyLisener.js
+++ client/src/Components/MyPage/MyRoom/MyLisener.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const MyLisener = () => {
-  return <div>Lisener</div>;
+  return (
+    <>
+      <div>이용권 정보 조회</div>
+      <div>총 재생시간</div>
+      <div>청취 곡수</div>
+      <div>Recently played</div>
+      <div>My favorite</div>
+      <div>나의 재생목록</div>
+    </>
+  );
 };
 
 export default MyLisener;
