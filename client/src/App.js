10b3e8be12c26b80cfe3fcb586924a4c4cee5f0c
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-03-07 23:20:53 +0900

    main from to ysh merge

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,15 @@ import Legister from "./Components/register/index";
 import Lisener from "./Components/register/user/Lisener";
 import Artist from "./Components/register/user/Artist";
 import Metamask from "./Web3/Metamask";
+import MyPageLayout from "./Components/MyPage/Index";
+import MyArtist from "./Components/MyPage/MyRoom/MyArtist";
+import MyLisener from "./Components/MyPage/MyRoom/MyLisener";
 
 function App() {
-
   useEffect(() => {
     Metamask.enableEthereum();
-  }, [])
-  
+  }, []);
+
   return (
     <>
       <Routes>
@@ -21,6 +23,9 @@ function App() {
           <Route path="legister" element={<Legister />}></Route>
           <Route path="Lisener" element={<Lisener />}></Route>
           <Route path="Artist" element={<Artist />}></Route>
+          <Route path="MyPageLayout" element={<MyPageLayout />}></Route>
+          <Route path="MyArtist" element={<MyArtist />}></Route>
+          <Route path="MyLisener" element={<MyLisener />}></Route>
         </Route>
       </Routes>
     </>
---
+++ client/src/Components/MyPage/MyRoom/MyLisener.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyLisener = () => {
+  return <div>Lisener</div>;
+};
+
+export default MyLisener;
