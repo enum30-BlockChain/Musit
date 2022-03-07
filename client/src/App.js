bec4a01c0ed1fe05d9bc3abc75dc38202402b487
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-03-07 17:42:08 +0900

    create metamask.ts and  methods

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import LegisterLayout from "./Components/Index";
 import Legister from "./Components/register/index";
 import Lisener from "./Components/register/user/Lisener";
 import Artist from "./Components/register/user/Artist";
+import Metamask from "./Web3/Metamask";
 
 function App() {
+
+  useEffect(() => {
+    Metamask.enableEthereum();
+  }, [])
+  
   return (
     <>
       <Routes>
