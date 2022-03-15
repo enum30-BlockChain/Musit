be6d42bcffffb89a3568a80813233409812cfaaa
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-03-15 11:59:40 +0900

    user invalide check coding and mysql add false or true

--- server/routes/users.js
+++ server/routes/users.js
@@ -25,12 +25,21 @@ router.post("/signin", async (req, res, next) => {
 router.post("/signup", async (req, res, next) => {
   try {
     console.log("signup을 server에 요청하였습니다.");
-    await Users.create({
-      address: req.body.address,
-      myfavorite: req.body.myfavorite,
-      nation: req.body.nation,
+    const user = await Users.findOne({
+      where: {
+        address: req.body.address,
+      },
     });
-    res.send("Created successfully");
+    if (user) {
+      res.send("Already Existed");
+    } else {
+      await Users.create({
+        address: req.body.address,
+        myfavorite: req.body.myfavorite,
+        nation: req.body.nation,
+      });
+      res.send("Created successfully");
+    }
   } catch (err) {
     console.error(err);
   }
