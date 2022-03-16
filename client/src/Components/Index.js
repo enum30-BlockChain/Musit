fa5b83913f832e1b55ec68025f66e6d2422318fa
ming <euphratesmin@gmail.com>
2022-03-16 10:26:43 +0900

    BackGround Wave

--- client/src/Components/Index.js
+++ client/src/Components/Index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Hoveringcard from "./contents/Hoveringcard";
+import Signup from "./register/signup";
 
 export { default as Lisener } from "./register/user/Lisener";
 export { default as Artist } from "./register/user/Artist";
@@ -7,10 +9,13 @@ export { default as Artist } from "./register/user/Artist";
 export const LegisterLayout = () => {
   return (
     <>
+      <></>
       <Link to="/Legister">
         <button>Legister</button>
       </Link>
       <div>메인페이지입니다.</div>
+      <Hoveringcard></Hoveringcard>
+      <Signup></Signup>
     </>
   );
 };
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return;
+
+  {
+    /* <img class="logo"> */
+  }
+
+  <div>navbar 로고랑 들어갈꺼야 </div>;
+  // <button class="loginButton">login</button>;
+}
+
+export default Navbar;
