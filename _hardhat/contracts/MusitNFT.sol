1ade00654f22c9f68a1909205e7c0c159c50f2f5
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-03-22 16:00:19 +0900

    add ERC721URIStorage and edit minting function

--- _hardhat/contracts/MusitNFT.sol
+++ _hardhat/contracts/MusitNFT.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -9,8 +10,10 @@ import "@openzeppelin/contracts/utils/Counters.sol";
   TODO: 토큰 uri 설정 함수, 토큰 uri 출력 함수, 출금 함수
 */
 
-contract MusitNFT is ERC721, Ownable {
+contract MusitNFT is ERC721URIStorage, Ownable {
   using Counters for Counters.Counter;
+
+  event MintMusitNFT (uint256 indexed tokenId, string tokenURI);
 
   uint256 public mintPrice; // 민팅 가격
   uint256 public totalSupplied; // 현재까지 발행된 총 수량
@@ -18,16 +21,16 @@ contract MusitNFT is ERC721, Ownable {
   uint256 public maxMintsPerWallet; // 지갑 당 민팅 총 수량
   uint256 public maxMintsPerTx; // 트랜잭션 당 민팅 총 수량
   bool public isMintEnabled;  // 민팅 가능 여부 결정
-  Counters.Counter private tokenId;
+  Counters.Counter private tokenId; // 발행할 NFT 토큰 Id
 
-  mapping (address => uint) mintsPerWallet;
+  mapping (address => uint256) mintsPerWallet; // 사용자 현재까지 완료한 민팅 개수
+  mapping (address => mapping(uint256 => uint256)) ownedNFT; // 주소가 소유한 NFT : 주소 => (tokenId => tokenURI)
 
   constructor () ERC721("Musit NFT","MUSIT") {
-    mintPrice = 0.01 ether;
+    mintPrice = 1 ether;
     maxSupply = 20;
     maxMintsPerWallet = 2;
     maxMintsPerTx = 1;
-    _baseURI();
   }
 
   function setMintPrice (uint256 _mintPrice) external onlyOwner{
@@ -50,22 +53,19 @@ contract MusitNFT is ERC721, Ownable {
     isMintEnabled = _isMintEnabled;
   }
 
-  function minting (uint256 _amount) external payable returns (uint256[] memory) {
+  function minting (string memory _tokenURI) external payable {
     require(isMintEnabled, "Mint system is not enabled yet.");
     require(msg.value == mintPrice, "Wrong value sent.");
-    require(_amount <= maxMintsPerTx);
-    require(totalSupplied + _amount <= maxSupply, "Exceed total supply. Please adjust mint amount and try again.");
-    require(mintsPerWallet[msg.sender] + _amount <= maxMintsPerWallet);
-
-    uint256[] memory newTokenIdList;
+    require(totalSupplied <= maxSupply, " Sold out!");
+    require(mintsPerWallet[msg.sender] <= maxMintsPerWallet, "Exceed total supply.");
     
-    for(uint256 i = 0; i < _amount; i++) {
-      tokenId.increment();
-      uint256 newTokenId = tokenId.current();
-      newTokenIdList[i] = newTokenId;
-      _safeMint(msg.sender, newTokenId);
-    }
-
-    return newTokenIdList;
+    tokenId.increment();
+    uint256 newTokenId = tokenId.current();
+    _safeMint(msg.sender, newTokenId);
+    _setTokenURI(newTokenId, _tokenURI);
+    emit MintMusitNFT(newTokenId, _tokenURI);
+    totalSupplied = newTokenId;
   }
+
+  
 }
