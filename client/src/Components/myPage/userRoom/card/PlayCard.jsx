7ee61981c27115e5b2a5c2007815f53b89bf71bf
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-03-23 22:19:51 +0900

    file make up and MyplayList likeMusic delete account MusicSearch like connect sucess so  mysql db connect delete check sucess

--- client/src/Components/myPage/userRoom/card/PlayCard.jsx
+++ client/src/Components/myPage/userRoom/card/PlayCard.jsx
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import PlayDetailCard from "./PlayDetailCard";
 
-const PlayCard = ({ id, name, genre, title, ipfs, select, setSelect }) => {
+const PlayCard = ({
+  id,
+  name,
+  genre,
+  title,
+  audio,
+  select,
+  setSelect,
+  like,
+  address,
+  checkBox,
+}) => {
   const [detail, setDetail] = useState([]);
+  const [likeCount, setLikeCount] = useState(like);
+  const [checkedInputs, setCheckedInputs] = useState();
+
+  useEffect(() => {
+    setCheckedInputs(checkBox);
+  }, []);
 
   const ArtistOnclick = (e) => {
     setSelect(e.target.name);
@@ -16,17 +33,45 @@ const PlayCard = ({ id, name, genre, title, ipfs, select, setSelect }) => {
       setDetail(res.data);
     });
   };
+
+  const changeHandler = async (checked) => {
+    const url = "http://localhost:5000/music/like";
+    const response = await axios
+      .post(url, { address, like, audio })
+      .then((res) => {})
+      .catch((err) => alert("회원가입부터하세용.", err));
+    if (checked) {
+      setCheckedInputs(true);
+      setLikeCount(likeCount + 1);
+    } else {
+      setCheckedInputs(false);
+      setLikeCount(likeCount - 1);
+    }
+  };
+
   return (
     <>
       <div>
         <li>{id}</li>
         <li>
-          <button name={ipfs} onClick={ArtistOnclick}>
+          <button name={audio} onClick={ArtistOnclick}>
             {title}
           </button>
         </li>
         <li>{name}</li>
         <li>{genre}</li>
+        <li>
+          <td>
+            <input
+              type="checkbox"
+              onChange={(e) => {
+                changeHandler(e.currentTarget.checked);
+              }}
+              checked={checkedInputs}
+            />
+            {likeCount}
+          </td>
+        </li>
         <button onClick={DetailOnClick}>상세보기</button>
       </div>
       {detail.map((DetailList, index) => (
--- client/src/Components/myPage/userRoom/page/MyPlayList.jsx
+++ client/src/Components/myPage/userRoom/page/MyPlayList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useOutletContext } from "react-router-dom";
-import "./MyPlayList.css";
+import "./css/MyPlayList.css";
 import PlayCard from "../card/PlayCard";
 const MyPlayList = () => {
   const [likesong, setLikesong] = useState([]);
@@ -25,17 +25,21 @@ const MyPlayList = () => {
           <li>제목</li>
           <li>가수명</li>
           <li>장르명</li>
+          <li>like</li>
         </div>
         {likesong.map((Song, index) => (
           <PlayCard
             id={index}
             key={index}
+            title={Song.title}
             name={Song.artist_name}
             genre={Song.Genre}
-            title={Song.title}
-            ipfs={Song.ipfs_hash}
+            like={Song.MusicLikes.length}
+            audio={Song.ipfs_hash}
             setSelect={setSelect}
             select={select}
+            address={address}
+            checkBox={likesong}
           />
         ))}
       </div>
