5ec9b61d05a1d01681803320753747deeb9edbf5
limcuck <89626659+POcodingWER@users.noreply.github.com>
2022-03-28 11:47:08 +0900

    1

--- client/src/Components/createNFT/CreateNFT.jsx
+++ client/src/Components/createNFT/CreateNFT.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
-import { Box, Stack, Typography } from '@mui/material'
+import React, {useState}from 'react'
+import { Box, Stack, Button, Typography, Card,CardActionArea,CardMedia,CardContent } from '@mui/material'
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { display } from '@mui/system';
+
 
 function CreateNFT() {
+  const [albumCoverImgFile, setAlbumCoverImgFile] = useState()
+  const [text, setText] = useState("이미지를 넣어주세요")
+  const handleChangeImage = (e)=>{
+    setAlbumCoverImgFile(e.target.files[0])
+  }
+  
   return (
     <Stack
       sx={{ display: "flex" }}
@@ -10,13 +19,32 @@ function CreateNFT() {
       alignItems="center"
       spacing={0.5}
     >
-      <Box sx={{mx:"auto",p:0}}>
+      <Box sx={{ mx: "auto", mt: 3, p: 0 }}>
         <Typography variant="h3">Responsive h3</Typography>
         <Box>
-          <div>createNFT</div>
+          <Typography sx={{ mt: 3 }} variant="h5">
+            Image, Video, Audio, or 3D Model
+          </Typography>
         </Box>
-        <Box>
-          <div>createNFT</div>
+        <Box sx={{mt:3,}}>
+          <Button component="label">
+          <Box sx={{ height:345,width:345, p: 2, border: "4px dashed grey" }}>
+        {albumCoverImgFile ? '' : <CloudUploadIcon sx={{color:'gray', height:"100%",width:"100%",  }} />}
+          {albumCoverImgFile && (
+            <img
+              src={URL.createObjectURL(albumCoverImgFile)}
+              style={{ width: "100%",height:"100%" }}
+            ></img>
+          )}
+            <input
+              id={"file-input"}
+              style={{ display: "none" }}
+              type="file"
+              name="imageFile"
+              onChange={(e) => handleChangeImage(e)}
+            />
+          </Box><p/>
+          </Button>
         </Box>
         <Box>
           <div>createNFT</div>
