3acd25cd6bf813ec69a7f3a40ad24eb815eb3438
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-03-30 13:42:18 +0900

    change folder name from uppercase to lowercase

--- client/src/Components/main/auction/Auction.jsx
+++
@@ -1,8 +0,0 @@
-import "./Auction.css"
-import React from 'react'
-
-export const Auction = () => {
-  return (
-    <div>Auction</div>
-  )
-}
--- client/src/Components/main/create/Create.jsx
+++
@@ -1,6 +0,0 @@
-import "./Create.css";
-import React from "react";
-
-export const Create = () => {
-  return <div>Collection</div>;
-};
