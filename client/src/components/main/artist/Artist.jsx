3cac98d50ee648cbf30eaa1d1f651b621abbe469
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-03-30 13:49:09 +0900

    change folder name from uppercase to lowercase

---
+++ client/src/components/main/artist/Artist.jsx
@@ -0,0 +1,10 @@
+import "./Artist.css";
+import React from "react";
+import { Artistcontentbox } from "./Artistcontentbox/Artistcontentbox";
+export const Artist = () => {
+  return (
+    <>
+      <Artistcontentbox />
+    </>
+  );
+};
