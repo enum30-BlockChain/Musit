6b4655394161aefe1ff72569877831db412fab5f
ming <euphratesmin@gmail.com>
2022-03-31 16:13:07 +0900

    auction duration

--- client/src/components/main/auction/Auctionupload.jsx
+++ client/src/components/main/auction/Auctionupload.jsx
@@ -1,9 +1,9 @@
 import "./Auctionupload.css";
 import React, { useState } from "react";
-// import LocalizationProvider from "@mui/lab/LocalizationProvider";
-// import AdapterDateFns from "@mui/lab/AdapterDateFns";
-// import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
-// import { TextField } from "@mui/material";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import AdapterDateFns from "@mui/lab/AdapterDateFns";
+import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
+import { TextField, Input } from "@mui/material";
 
 export const Auctionupload = () => {
   //앨범커버 이미지 가져오기
@@ -13,34 +13,34 @@ export const Auctionupload = () => {
 
   return (
     <>
+      <h1 className="auction-title">
+        Put your work up for Musit X Enun30 Auction
+      </h1>
       <div className="auction-layout">
-        <h1> Put your work up for Musit X Enun30 auction </h1>
-        <div className="auction-box">
+        <div className="auction-imgbox">
           <h1>Album Cover</h1>
-          <div className="auction-imgbox">
-            <img
-              src="https://cdn.univ20.com/wp-content/uploads/2018/10/ea6345acc53bb8cbac3564f661ae9dd4.png"
-              alt="auction-cover"
-              id="auction-albumcover"
+          <img
+            src="https://cdn.univ20.com/wp-content/uploads/2018/10/ea6345acc53bb8cbac3564f661ae9dd4.png"
+            alt="auction-cover"
+            id="auction-albumcover"
+          />
+        </div>
+        <div className="auction-box">
+          <h2>Price (Ethereum only)</h2>
+          <Input type="text" placeholder="Amount" sx={{ width: 255 }}></Input>
+          <h2>Duration</h2>
+          <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DesktopDatePicker
+              label={"Acution Duration"}
+              value={auctionStartDate}
+              onChange={(newValue) => {
+                setAuctionStsrtDate(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
             />
-          </div>
-          <div className="auction-inputbox">
-            <h2>Price</h2>
-            <input type="text" placeholder="Amount"></input>
-            <h2>Duration</h2>
-            {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
-              <DesktopDatePicker
-                label={"Acution Duration"}
-                value={auctionStartDate}
-                onChange={(newValue) => {
-                  setAuctionStsrtDate(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider> */}
+          </LocalizationProvider>
 
-            <></>
-          </div>
+          <></>
         </div>
       </div>
     </>
