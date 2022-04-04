73e415bd2f5818c7c86f3c567d62f24f5d917e0f
ming <euphratesmin@gmail.com>
2022-04-05 02:03:54 +0900

    edite

--- client/src/components/landingpage/LandingMainPage.jsx
+++ client/src/components/landingpage/LandingMainPage.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function LandingMainPage() {
-  return <div>LandingMainPage</div>;
+  return (
+    <div>
+      <div>
+        <p>We are NFT</p>
+        <p>We are Music</p>
+        <p>We are BlockChain Revolution</p>
+        <p>We are ENUM30 X MUSIT</p>
+      </div>
+    </div>
+  );
 }
