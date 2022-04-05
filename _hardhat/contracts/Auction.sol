4bc21740e462441b97a161847090658a7a53b6de
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-04-05 14:26:25 +0900

    Add bid cultimulating system in Auction contract

--- _hardhat/contracts/Auction.sol
+++ _hardhat/contracts/Auction.sol
@@ -4,17 +4,18 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Auction is ReentrancyGuard {
+contract Auction is ReentrancyGuard, Ownable {
   using Counters for Counters.Counter;
 
-  Counters.Counter public ItemCounter;
+  Counters.Counter public itemCounter;
   address payable public immutable feeAccount;
   uint public immutable feePercent;
   uint public minBidAmount;
 
   mapping (uint => Item) public items; // itemId => 경매아이템 : 경매에 올린 아이템 리스트
-  mapping (uint => mapping (address => uint )) public pendingBids; // itemId => ( bidder => bid ) : 입찰하려고 올린 금액들
+  mapping (uint => mapping (address => uint)) public pendingBids; // itemId => ( bidder => bid ) : 입찰하려고 올린 금액들
 
   enum StatusType { ENROLLED, CLOSED, CANCELLED }
 
@@ -51,18 +52,28 @@ contract Auction is ReentrancyGuard {
     minBidAmount = 1000 wei;
   }
 
+  /* Modifier declaration */
+  modifier onlyNftOwner (IERC721 _nft, uint _tokenId) {
+    require(msg.sender == _nft.ownerOf(_tokenId), "Only owner can enroll nft");
+    _;
+  }
+  modifier onlySeller (uint _itemId) {
+    require(msg.sender == items[_itemId].seller, "Only seller can end it.");
+    _;
+  }
+
   /* Function declaration */
   // 경매 아이템 등록 및 경매 시작 함수
-  function enroll(uint _startPrice, uint _endAt, IERC721 _nft, uint _tokenId ) external nonReentrant {
+  function enroll(uint _startPrice, uint _endAt, IERC721 _nft, uint _tokenId ) 
+    external nonReentrant onlyNftOwner(_nft, _tokenId) {
     require(_startPrice >= minBidAmount, "Should start price is bigger than mininum bid amount");
-    require(msg.sender == _nft.ownerOf(_tokenId), "Only owner can enroll nft");
     require(block.timestamp < _endAt, "Cannot set end time as past time");
-    ItemCounter.increment();
-    uint _itemId = ItemCounter.current();
+    itemCounter.increment();
+    uint _itemId = itemCounter.current();
     items[_itemId] = Item(
       _startPrice, 
       block.timestamp,
-      _endAt, 
+      _endAt/1000, 
       _tokenId, 
       payable(msg.sender),
       address(0),
@@ -78,34 +89,40 @@ contract Auction is ReentrancyGuard {
 
   // 경매 참여 함수
   function bid(uint _itemId) external payable nonReentrant {
+    require(_itemId <= itemCounter.current(), "This item is not enrolled");
     Item storage auctionItem = items[_itemId];
-    require(block.timestamp >= auctionItem.startAt, "Auction isn't started yet");
     require(block.timestamp < auctionItem.endAt, "Auction is ended");
     require(auctionItem.status == StatusType.ENROLLED, "This auction is ended or cancelled");
-    require(msg.value > calPriceWithFee(auctionItem.topBid) + minBidAmount,
+    require(removeFee(msg.value) + pendingBids[_itemId][msg.sender] >= auctionItem.topBid + minBidAmount,
       "Bid amount should be bigger than prev top bid as much as minumum bid amount");
 
     if(auctionItem.topBidder != address(0)) {
-      pendingBids[_itemId][msg.sender] += msg.value;
+      pendingBids[_itemId][auctionItem.topBidder] += auctionItem.topBid;
     }
 
+    if(pendingBids[_itemId][msg.sender] > 0) {
+      // 입찰을 이미 했던 사람은 총 입찰가격에 더해줌
+      auctionItem.topBid = pendingBids[_itemId][msg.sender] + removeFee(msg.value);
+    } else {
+      auctionItem.topBid = removeFee(msg.value);
+    }
     auctionItem.topBidder = msg.sender;
-    auctionItem.topBid = removeFee(msg.value);
 
     emit Bid(_itemId, msg.sender, auctionItem.topBid);
   }
 
-  function end(uint _itemId) external {
+  // 경매 종료 함수
+  function end(uint _itemId) external nonReentrant onlySeller(_itemId) {
     Item storage auctionItem = items[_itemId];
     require(auctionItem.status == StatusType.ENROLLED, "This item hasn't been enrolled");
     require(block.timestamp > auctionItem.endAt, "It is not the time to close auction");
-    require(msg.sender == auctionItem.seller, "Only seller can end it.");
     auctionItem.status = StatusType.CLOSED;
 
     if (auctionItem.topBidder != address(0)) {
       auctionItem.nft.transferFrom(address(this), auctionItem.topBidder, auctionItem.tokenId);
       auctionItem.seller.transfer(auctionItem.topBid); // 수수료 제외한 나머지 판매자에게 전송
       feeAccount.transfer((auctionItem.topBid * feePercent) / 100); // 수수료는 배포자에게 전송
+      pendingBids[_itemId][auctionItem.topBidder] = 0;  // 입찰자는 출금을 못하도록 0으로 바꿈
     } else {
       auctionItem.nft.transferFrom(address(this), auctionItem.seller, auctionItem.tokenId);
     }
@@ -113,7 +130,26 @@ contract Auction is ReentrancyGuard {
     emit End(_itemId, auctionItem.topBidder, auctionItem.topBid);
   }
 
-  function cancel(uint _itemId) external {
+  // 경매 강제 종료 함수
+  function forceEnd(uint _itemId)  external nonReentrant onlyOwner  {
+    Item storage auctionItem = items[_itemId];
+    require(auctionItem.status == StatusType.ENROLLED, "This item hasn't been enrolled");
+    auctionItem.status = StatusType.CLOSED;
+
+    if (auctionItem.topBidder != address(0)) {
+      auctionItem.nft.transferFrom(address(this), auctionItem.topBidder, auctionItem.tokenId);
+      auctionItem.seller.transfer(auctionItem.topBid); // 수수료 제외한 나머지 판매자에게 전송
+      feeAccount.transfer((auctionItem.topBid * feePercent) / 100); // 수수료는 배포자에게 전송
+      pendingBids[_itemId][auctionItem.topBidder] = 0;  // 입찰자는 출금을 못하도록 0으로 바꿈
+    } else {
+      auctionItem.nft.transferFrom(address(this), auctionItem.seller, auctionItem.tokenId);
+    }
+    
+    emit End(_itemId, auctionItem.topBidder, auctionItem.topBid);
+  }
+
+  // 경매 입찰 참여자가 없으면 경매 취소할 수 있는 함수
+  function cancel(uint _itemId) external nonReentrant {
     Item storage auctionItem = items[_itemId];
     require(msg.sender == auctionItem.seller, "Only seller can cancel it");
     require(auctionItem.status == StatusType.ENROLLED , "It is already started or ended");
@@ -125,12 +161,26 @@ contract Auction is ReentrancyGuard {
     emit Cancel(_itemId, msg.sender);
   }
 
+  // 강제 취소 함수
+  function forceCancel(uint _itemId) external nonReentrant onlyOwner {
+    Item storage auctionItem = items[_itemId];
+    require(auctionItem.status == StatusType.ENROLLED , "It is already started or ended");
+    auctionItem.status = StatusType.CANCELLED;
+
+    payable(auctionItem.topBidder).transfer(auctionItem.topBid);
+    auctionItem.nft.transferFrom(address(this), auctionItem.seller, auctionItem.tokenId);
+
+    emit Cancel(_itemId, msg.sender);
+  }
+
   // pending bids 출금 함수
-  function withdraw(uint _itemId) external {
+  function withdraw(uint _itemId) external nonReentrant {
     require(msg.sender != items[_itemId].topBidder);
     uint balance = pendingBids[_itemId][msg.sender];
     require(balance != 0, "Nothing to withdraw");
     pendingBids[_itemId][msg.sender] = 0;
+
+    payable(msg.sender).transfer(balance);
 
     emit Withdraw(_itemId, msg.sender, balance);
   }
--- _hardhat/test/auction.test.ts
+++ _hardhat/test/auction.test.ts
@@ -1,6 +1,6 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { BigNumber, BigNumberish, ContractFactory } from "ethers";
+import { BigNumber, BigNumberish, ContractFactory, Transaction } from "ethers";
 import { ethers } from "hardhat";
 import { Auction, MusitNFT } from "../typechain";
 
@@ -52,8 +52,8 @@ describe("Auction contract", () => {
       await musitNFT.connect(addr1).minting(tokenURI, {value : ethToWei(mintPrice)})
       await musitNFT.connect(addr1).approve(auction.address, 1);
       startPrice = 1000
-      startAt = Math.floor(Date.now() / 1000)
-      endAt =  startAt + 120 // 시작후 1분 뒤 종료
+      startAt = Date.now()
+      endAt =  startAt + 60000 // 시작후 1분 뒤 종료
       nft = musitNFT.address;
     })
 
@@ -61,14 +61,14 @@ describe("Auction contract", () => {
       it("New enrolled item attributes test", async () => {
         expect(await musitNFT.ownerOf(1)).to.equal(addr1.address)
   
-        await expect(await await auction.connect(addr1).enroll(startPrice, endAt, nft, 1))
+        await expect(await auction.connect(addr1).enroll(startPrice, endAt, nft, 1))
           .emit(auction, "Enrolled")
           .withArgs(1, startPrice, musitNFT.address, 1, addr1.address);
         
         const item = await auction.items(1)
         expect(item.startPrice).to.equal(startPrice);
         // expect(item.startAt).to.equal(startAt); // 블록 타임스탬프와 현재 시간과 차이가 있음 : hardhat에서는 보통 10초 내외
-        expect(item.endAt).to.equal(endAt);
+        expect(item.endAt).to.equal(Math.floor(endAt/1000));
         expect(item.tokenId).to.equal(1);
         expect(item.seller).to.equal(addr1.address);
         expect(item.topBidder).to.equal(ethers.constants.AddressZero);
@@ -81,8 +81,6 @@ describe("Auction contract", () => {
     describe("bid", async() => {
 
       beforeEach(async () => {
-        startPrice = 1000
-        endAt = Math.floor(Date.now() / 1000) +  60 // 시작후 60초 뒤 종료
         await auction.connect(addr1).enroll(startPrice, endAt, nft, 1)
       })
       
@@ -104,34 +102,77 @@ describe("Auction contract", () => {
           expect((await auction.items(1)).status).to.equal(2); // cancelled
         });
 
-        // it("end status", async () => {
-        //   expect((await auction.items(1)).status).to.equal(0);
-        //   expect((await auction.items(1)).topBid).to.equal(startPrice);
+        it("end status", async () => {
+          expect((await auction.items(1)).status).to.equal(0);
+          expect((await auction.items(1)).topBid).to.equal(startPrice);
 
-        //   await expect(auction.connect(addr1).end(1)).to.be.revertedWith(
-        //     "It is not the time to close auction"
-        //   );
-        //   expect((await auction.items(1)).status).to.equal(0);
+          await expect(auction.connect(addr1).end(1)).to.be.revertedWith(
+            "It is not the time to close auction"
+          );
+          expect((await auction.items(1)).status).to.equal(0);
 
-        //   await delay(15000);
-
-        //   await auction.connect(addr1).end(1);
-        //   expect((await auction.items(1)).status).to.equal(1);
-        // });
+          await auction.connect(deployer).forceEnd(1);
+          expect((await auction.items(1)).status).to.equal(1);
+        });
       })
       
       describe("Bidding test", async() => {
-        it("top bid and to bidder",async () => {
-          // expect(await (await auction.items(1)).endAt).to.equal(await auction.connect(addr1).getBlockTimestamp())
-          await auction.connect(addr2).bid(1, {value: auction.calPriceWithFee(3000)});
-          expect((await auction.items(1)).topBid).to.equal(3000)
-          expect((await auction.items(1)).topBidder).to.equal(addr2.address)
-          await auction.connect(addr3).bid(1, {value: auction.calPriceWithFee(4000)});
-          expect((await auction.items(1)).topBid).to.equal(4000)
-          expect((await auction.items(1)).topBidder).to.equal(addr3.address)
+        let initAddr1Balance: BigNumber;
+        let initAddr2Balance: BigNumber;
+        let initAddr3Balance: BigNumber;
+        let finalAddr1Balance: BigNumber;
+        let finalAddr2Balance: BigNumber;
+        let finalAddr3Balance: BigNumber;
+        
+        beforeEach(async () => {
+          await (await auction.connect(addr2).bid(1, {value: await auction.calPriceWithFee(2000)})).wait();
+          await (await auction.connect(addr3).bid(1, {value: await auction.calPriceWithFee(3000)})).wait();
         })
-        it("revert test", async() => {
+
+        it("top bid and to bidder",async () => {
+          initAddr1Balance = await addr1.getBalance()
+          initAddr2Balance = await addr2.getBalance()
+          initAddr3Balance = await addr3.getBalance()
+
+          expect(await auction.pendingBids(1, addr2.address)).to.equal(2000);
+          expect((await auction.items(1)).topBid).to.equal(3000);
+          expect((await auction.items(1)).topBidder).to.equal(addr3.address);
+          expect(await auction.pendingBids(1, addr2.address)).to.equal(2000);
+          expect(await auction.pendingBids(1, addr3.address)).to.equal(0);
+          await auction.connect(addr2).bid(1, {value: auction.calPriceWithFee(1500)})
           
+
+          // 경매 강제 종료
+          await auction.connect(deployer).forceEnd(1);
+
+          expect(auction.pendingBids(1, addr3.address)).to.equal(3000);
+
+          // 경매 종료 후 판매자 잔고 증가 확인
+          finalAddr1Balance = initAddr1Balance.add((await auction.items(1)).topBid);
+          expect(await addr1.getBalance()).to.equal(finalAddr1Balance);
+
+          // 출금 후 구매자 잔고 감소 확인
+          // let addr2PendingBid = await auction.calPriceWithFee((await auction.pendingBids(1, addr2.address)))
+          // finalAddr2Balance = initAddr2Balance.sub(addr2PendingBid);
+          // let tx = await (await auction.connect(addr2).withdraw(1)).wait();
+          // expect(await addr2.getBalance()).to.equal(finalAddr2Balance.sub(tx.cumulativeGasUsed));
+          // 가스비 때문에 계속 fail..
+
+          // 출금 후 팬딩 금액 변화 확인
+          expect(await auction.pendingBids(1, addr2.address)).to.equal(2000)
+          await auction.connect(addr2).withdraw(1)
+          expect(await auction.pendingBids(1, addr2.address)).to.equal(0)
+
+          // 
+        })
+        
+        it("revert test", async() => {
+          await expect(auction.connect(addr2).bid(2, {value: await auction.calPriceWithFee(2000)})).revertedWith("This item is not enrolled")
+          await expect(auction.connect(addr2).bid(1, {value: await auction.calPriceWithFee(1100)})).revertedWith("Bid amount should be bigger than prev top bid as much as minumum bid amount")
+          await expect(auction.connect(addr2).bid(1, {value: await auction.calPriceWithFee(900)})).revertedWith("Bid amount should be bigger than prev top bid as much as minumum bid amount")
+          await expect(auction.connect(addr2).bid(1, {value: await auction.calPriceWithFee(900)})).revertedWith("Bid amount should be bigger than prev top bid as much as minumum bid amount")
+          await auction.connect(deployer).forceCancel(1);
+          await expect(auction.connect(addr2).bid(1, {value: await auction.calPriceWithFee(2000)})).revertedWith("This auction is ended or cancelled")
         })
       })
       
