58268520bf68b2567b41857afbba000bcabf4d29
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-04-11 16:31:41 +0900

    fixed side nav bar height problem

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -8,6 +8,29 @@ import { Dashboard } from "./dashboard/Dashboard";
 import { Searchbar } from "./searchbar/Searchbar";
 
 export const Main = () => {
+  useEffect(() => {
+    sidebarToggle();
+  }, [])
+
+  const sidebarToggle = () => {
+    const sidebarToggle = document.querySelector(".sidebar-toggle");
+    const sidebar = document.querySelector("nav");
+
+    let getMenuStatus = localStorage.getItem("menu_status");
+    if (getMenuStatus && getMenuStatus === "close") {
+      sidebar.classList.toggle("close");
+    }
+
+    sidebarToggle.addEventListener("click", () => {
+      sidebar.classList.toggle("close");
+      if (sidebar.classList.contains("close")) {
+        localStorage.setItem("menu_status", "close");
+      } else {
+        localStorage.setItem("menu_status", "open");
+      }
+    });
+  }
+
   return (
     <section className="main">
       <Searchbar />
