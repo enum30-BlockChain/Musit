6ff0a57c8d24f6881a6bc82961d890d70c41b1f4
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-04-11 12:33:53 +0900

    dashboard delete check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,13 @@ import "./App.css";
 import { Navbar } from "./components/navbar/Navbar";
 import Back from "./components/landingpage/back/Back";
 import React from "react";
-// import { Main } from "./components/main/Main";
+import { Main } from "./components/main/Main";
 
 const App = () => {
   return (
     <>
       <Back />
-      {/* <Main /> */}
+      <Main />
       <Navbar />
     </>
   );
