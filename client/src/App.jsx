6cf82b3f5f7809c5d3d428ab31f2d127216e8102
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-04-11 14:20:50 +0900

    delete page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,11 @@ import "./App.css";
 import { Navbar } from "./components/navbar/Navbar";
 import Back from "./components/landingpage/back/Back";
 import React from "react";
-import { Main } from "./components/main/Main";
 
 const App = () => {
   return (
     <>
       <Back />
-      <Main />
       <Navbar />
     </>
   );
