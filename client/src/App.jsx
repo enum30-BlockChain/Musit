510fd226b46688085ab68954e2aa03dd5103847f
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-04-11 11:50:47 +0900

    main2 commit

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { readArtistList, readArtistData } from "./redux/actions/artistActions";
+import { readMetamaskData } from "./redux/actions/metamaskAction";
+import { readMusicList } from "./redux/actions/musicActions";
+import { readUserData } from "./redux/actions/userActions";
+import Test from "./Test";
+import ArtistTest from "./ArtistTest";
+import UserTest from "./UserTest";
+
+function App() {
+	const user = useSelector((state) => state.user);
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const init = async () => {
+			await dispatch(readMetamaskData());
+			await dispatch(readUserData());
+			await dispatch(readArtistList());
+			await dispatch(readArtistData());
+			await dispatch(readMusicList());
+		};
+		init();
+	}, []);
+
+	if (user.loading) {
+		return <>스켈레톤, 스피너</>;
+	} else
+		return (
+			<div className="App">
+				{user.error ? (
+					<>유저없음</>
+				) : (
+					<div>
+						<div>{user.address}</div>
+						<div>{user.nickname}</div>
+						<UserTest />
+						<ArtistTest />
+						<Test />
+					</div>
+				)}
+			</div>
+		);
+}
+
+export default App;
