9649f110be9d75c7107d32ff8b29ae6b2941e788
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-04-11 14:23:59 +0900

    main add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { Navbar } from "./components/navbar/Navbar";
 import Back from "./components/landingpage/back/Back";
 import React from "react";
+import Main from "./components/main/Main";
 
 const App = () => {
   return (
     <>
       <Back />
+      <Main />
       <Navbar />
     </>
   );
