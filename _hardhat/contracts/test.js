43534801e665c22b8d737f511f2aa0cef998f407
jeonjinoung <89626182+jeonjinoung@users.noreply.github.com>
2022-04-11 10:20:16 +0900

    delete

--- _hardhat/contracts/test.js
+++
@@ -1,9 +0,0 @@
-const now = Date.now()
-console.log(now);
-
-const date1 = now + 60000
-const date2 = date1 + 60000 * 2
-console.log(new Date(date1))
-console.log(date1)
-console.log(new Date(date2))
-console.log(date2)
