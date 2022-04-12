9428b25d77e098f6fa06d6da214c59ce7deeae89
ming <euphratesmin@gmail.com>
2022-04-13 00:41:33 +0900

    artist info

--- client/src/components/main/mypage/subscription/Subscription.jsx
+++ client/src/components/main/mypage/subscription/Subscription.jsx
@@ -5,26 +5,23 @@ import Button from "@mui/material/Button";
 export const Subscription = () => {
   return (
     <>
-      <div className="sub-container">
-        <div className="sub-empty"></div>
-        <div className="sub-layout">
-          <h2 className="sub">Subscription Options</h2>
-          <div className="sub-box">
-            <div className="sub-card">
-              <h3 className="sub-index">1 Month Supscription</h3>
-              0.1 ethereum
-              <Button>BUY</Button>
-            </div>
-            <div className="sub-card">
-              <h3 className="sub-index">3 Months Supscription</h3>
-              0.3 ethereum
-              <Button>BUY</Button>
-            </div>
-            <div className="sub-card">
-              <h3 className="sub-index">6 Months Supscription</h3>
-              0.6 ethereum
-              <Button>BUY</Button>
-            </div>
+      <h2 className="sub">Subscription Options</h2>
+      <div className="sub-layout">
+        <div className="sub-box">
+          <div className="sub-card">
+            <h3 className="sub-index">1 Month Supscription</h3>
+            0.1 ethereum
+            <Button>BUY</Button>
+          </div>
+          <div className="sub-card">
+            <h3 className="sub-index">3 Months Supscription</h3>
+            0.3 ethereum
+            <Button>BUY</Button>
+          </div>
+          <div className="sub-card">
+            <h3 className="sub-index">6 Months Supscription</h3>
+            0.6 ethereum
+            <Button>BUY</Button>
           </div>
         </div>
       </div>
