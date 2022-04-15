32268eee3ec4be02a4249a98003dca9fba2afc10
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-04-15 16:41:01 +0900

    resolved playbar error and working on ordinary css

--- client/src/components/main/store/enroll/auction/Auction.jsx
+++ client/src/components/main/store/enroll/auction/Auction.jsx
@@ -13,6 +13,7 @@ const Auction = () => {
 	const musicData = useSelector((state) => state.music);
 
 	useEffect(async () => {
+		
 		await dispatch(readMusicData(ipfs_hash));
 	}, []);
   
@@ -22,7 +23,7 @@ const Auction = () => {
 	return musicData.loading || !musicData.ipfs_hash ? (
 		<LoadingContent/>
 	) : musicData.error ? (
-		<>Error</>
+		<ErrorContent/>
 	) : (
     <>
       <SuccessContent musicData={musicData} ipfs_hash={ipfs_hash} />
@@ -32,13 +33,13 @@ const Auction = () => {
 
 const LoadingContent = () => {
   return (
-		<section className="minting-layout">
-			<div className="content-box minting-img-box">
+		<section className="auction-layout">
+			<div className="content-box auction-img-box">
         <h2>Album Cover Image</h2>
 				<Skeleton width={400} height={400} sx={{marginTop:"20px"}} variant="circular" />
 			</div>
 
-			<div className="minting-content-container">
+			<div className="auction-content-container">
 				<div className="content-box title-box">
 					<h2 className="title">Title</h2>
           <Skeleton width={400} height={50} variant="text" />
@@ -56,29 +57,30 @@ const LoadingContent = () => {
 	);
 }
 
+const ErrorContent = () => {
+  return (
+		<section className="auction-layout">
+			ERROR
+		</section>
+	);
+}
+
 const SuccessContent = ({musicData, ipfs_hash}) => {
   const artistData = useSelector((state) => state.artist);
-  const mintingData = useSelector((state) => state.musitNFTMinting);
 	const dispatch = useDispatch();
 
-  const mintingOnClick = async () => {
-    const metadata = {
-			artist_address: artistData.user_address,
-			audio_ipfs_hash: ipfs_hash,
-		};
-
-    await dispatch(mintingMusitNFT(metadata))
-    await Ethers.minting(mintingData.data.path)
+  const enrollAuctionOnClick = async () => {
+    
   }
 
 	return (
-		<section className="minting-layout">
-			<div className="content-box minting-img-box">
+		<section className="auction-layout">
+			<div className="content-box auction-img-box">
 				<h2>Album Cover Image</h2>
 				<img src={musicData.img_file} />
 			</div>
 
-			<div className="minting-content-container">
+			<div className="auction-content-container">
 				<div className="content-box title-box">
 					<h2 className="title">Title</h2>
 					<p className="content">{musicData.title}</p>
@@ -90,9 +92,9 @@ const SuccessContent = ({musicData, ipfs_hash}) => {
 				<div className="content-box description-box">
 					<h2 className="title">Description</h2>
 					<p className="content">{musicData.description}</p>
-					<div className="minting-btn">
+					<div className="auction-btn">
 						<Button
-              onClick={mintingOnClick}
+              onClick={enrollAuctionOnClick}
 							sx={{
                 color: "var(--black-light-color)",
 								backgroundColor: "var(--box1-color)",
