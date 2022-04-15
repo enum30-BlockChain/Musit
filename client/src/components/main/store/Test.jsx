2d62cb7e76df1f4a9e933abb583a097fd31b901f
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-04-15 18:36:47 +0900

    start to create layout form

---
+++ client/src/components/main/store/Test.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Test.css"
+const Test = () => {
+  return (
+    <section className='layout'>
+      <div className="input-container">
+        
+      </div>
+      <div className="list-container">
+        
+      </div>
+    </section>
+  )
+}
+
+export default Test
