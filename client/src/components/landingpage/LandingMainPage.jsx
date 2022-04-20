5e638698525f2df481293a522147611b9a57215f
limcuck <89626659+POcodingWER@users.noreply.github.com>
2022-04-20 23:59:47 +0900

    main page 마무으리

--- client/src/components/landingpage/LandingMainPage.jsx
+++ client/src/components/landingpage/LandingMainPage.jsx
@@ -1,21 +1,49 @@
 import "./LandigMainPage.css";
-import React from "react";
+import React, { useEffect, useState,useRef } from "react";
 
-// import Button from "@mui/material/Button";
-// import Metamask from "./../../web3/Metamask";
-
-export default function LandingMainPage({ address }) {
-  // const connectOnclick = () => {
-  //   Metamask.connectWallet();
-  // };
-
-  // const sliceAddress =
-  //   address &&
-  //   address.substr(0, 5) + "..." + address.substr(address.length - 4, 4);
+export default function LandingMainPage() {
+  const audioPlayer = useRef();
+  const [name, setName] = useState("Tracks")
+  const musicBox=[
+    {title:"Jawbreaker - Do You Still Hate Me?" ,track:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/59639/rock.m4a"},
+    {title:"Judas Priest - Painkiller" , track:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/59639/metal.m4a"},
+    {title:"Stevie Wonder - For Once In My Life", track:"https://s3-us-west-2.amazonaws.com/s.cdpn.io/59639/soul.m4a"},
+  ]
+  const Tape =async (e)=>{
+    audioPlayer.current.src=musicBox[e.target.title].track;
+    await audioPlayer.current.play();
+  }
 
   return (
     <>
-      <h1 className="landing-layout">철순이 기타자리</h1>
+    <div class="container">
+    <div class="buff-jambox">
+        <div class="jambox">
+            <div class="speaker top-left"></div>
+            <div class="speaker top-right"></div>
+            <div class="speaker bottom-left"></div>
+            <div class="speaker bottom-right"></div>
+        </div>
+        <div class="arms">
+            <div class="arm left-arm">
+                <div class="bicep left-bicep"></div>
+                <div class="forearm left-forearm"></div>
+            </div>
+            <div class="arm right-arm">
+                <div class="bicep right-bicep"></div>
+                <div class="forearm right-forearm"></div>
+            </div>
+        </div>
+    </div>
+    <div class="tracks">
+        <h2 class="title">{name}</h2>
+        <a class="track" onClick={Tape} title='0'></a>
+        <a class="track" onClick={Tape} title='1'></a>
+        <a class="track" onClick={Tape} title='2'></a>
+        <audio ref={audioPlayer} />
+    </div>
+</div>
+
     </>
   );
 }
