70af38350029935c527677e335ae4c17b1dbcaf5
limcuck <89626659+POcodingWER@users.noreply.github.com>
2022-04-20 16:37:45 +0900

    artist 숫자 업데이트

--- client/src/components/main/artist/artistdashbord/dashbordcontent/Contentbox.jsx
+++ client/src/components/main/artist/artistdashbord/dashbordcontent/Contentbox.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import { useSelector, useDispatch } from "react-redux";
 import "./Contentbox.css";
 
 export const Contentbox = () => {
+  const artist = useSelector((state) => state.artist);
+  let totalMusicLikes = 0
+  let totalMusicPlayTime = 0
+  artist.Music.forEach(music=> totalMusicLikes += music.MusicLikes.length);
+  artist.Music.forEach((music)=> {  totalMusicPlayTime += music.play_count * music.play_time  });
+  console.log(artist)
   return (
     <div className="contentbox">
       <div className="title">
@@ -15,13 +21,13 @@ export const Contentbox = () => {
           {/* 아티스트가 받은 음악 좋아요 갯수 */}
           <i className="uil uil-thumbs-up"></i>
           <span className="text">Total Music Likes</span>
-          <span className="number">10</span>
+          <span className="number">{totalMusicLikes && totalMusicLikes}</span>
         </div>
         <div className="box box1">
           {/* 아티스트가 받은 좋아요 갯수 */}
           <i className="uil uil-heart"></i>
           <span className="text">Total Artist Likes</span>
-          <span className="number">10</span>
+          <span className="number">{artist && artist.ArtistLikes.length}</span>
         </div>
         <div className="box box1">
           {/* 아티스트 수입  */}
@@ -35,7 +41,9 @@ export const Contentbox = () => {
           {/* 음악 재생한 시간  */}
           <i className="uil uil-hourglass"></i>
           <span className="text">Total Music Played Time</span>
-          <span className="number">10</span>
+          {totalMusicPlayTime===0
+          ?<span className="number">0</span>
+          :<span className="number">{Math.floor(totalMusicPlayTime/ 60/60)}h {Math.floor(totalMusicPlayTime/60 % 60)}m {totalMusicPlayTime % 60}s</span>}
         </div>
         <div className="box box1">
           {/* 내가  재생한 ㅇ */}
@@ -47,7 +55,7 @@ export const Contentbox = () => {
           {/* 내가 업로드한 파일 갯수 */}
           <i className="uil uil-file-upload"></i>
           <span className="text">Music Upload count</span>
-          <span className="number">1</span>
+          <span className="number">{artist && artist.Music.length}</span>
         </div>
       </div>
     </div>
