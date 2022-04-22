25857e975a58ea7fea821965610e439f5952ec53
ming <euphratesmin@gmail.com>
2022-04-22 11:17:09 +0900

    genre

--- client/src/components/landingpage/LandingMainPage.jsx
+++ client/src/components/landingpage/LandingMainPage.jsx
@@ -1,59 +1,12 @@
 import "./LandigMainPage.css";
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 
 export default function LandingMainPage() {
-  const audioPlayer = useRef();
-  const [name, setName] = useState("Tracks");
-  const musicBox = [
-    {
-      title: "Jawbreaker - Do You Still Hate Me?",
-      track: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/59639/rock.m4a",
-    },
-    {
-      title: "Judas Priest - Painkiller",
-      track: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/59639/metal.m4a",
-    },
-    {
-      title: "Stevie Wonder - For Once In My Life",
-      track:
-        "https://raw.githubusercontent.com/ricardoolivaalonso/recursos/master/radio/radio.mp3",
-    },
-  ];
-  const Tape = async (e) => {
-    audioPlayer.current.src = musicBox[e.target.title].track;
-    await audioPlayer.current.play();
-  };
-
   return (
     <>
-      {/* <div class="container">
-    <div class="buff-jambox">
-        <div class="jambox">
-            <div class="speaker top-left"></div>
-            <div class="speaker top-right"></div>
-            <div class="speaker bottom-left"></div>
-            <div class="speaker bottom-right"></div>
-        </div>
-        <div class="arms">
-            <div class="arm left-arm">
-                <div class="bicep left-bicep"></div>
-                <div class="forearm left-forearm"></div>
-            </div>
-            <div class="arm right-arm">
-                <div class="bicep right-bicep"></div>
-                <div class="forearm right-forearm"></div>
-            </div>
-        </div>
-    </div>
-    <div class="tracks">
-        <h2 class="title">{name}</h2>
-        <a class="track" onClick={Tape} title='0'></a>
-        <a class="track" onClick={Tape} title='1'></a>
-        <a class="track" onClick={Tape} title='2'></a>
-        <audio ref={audioPlayer} />
-    </div>
-        
-</div> */}
+      <div className="code">
+        <h1>We are music</h1>
+      </div>
     </>
   );
 }
--- client/src/components/main/music/enummusic/scrollEvent/ScrollEvent.jsx
+++ client/src/components/main/music/enummusic/scrollEvent/ScrollEvent.jsx
@@ -6,7 +6,7 @@ import MainCardNav from "../../main/MainCardNav";
 import { useSelector } from "react-redux";
 import SimpleBackdrop from "../../SimpleBackdrop";
 
-const fakeFetch = (delay = 500) => new Promise(res => setTimeout(res, delay));
+const fakeFetch = (delay = 500) => new Promise((res) => setTimeout(res, delay));
 
 const ListItem = () => (
   <div className="ListItem">
@@ -14,67 +14,64 @@ const ListItem = () => (
   </div>
 );
 
-
 function ScrollEvent() {
-  const [lodingState,setLoadingState] = useState(true);
+  const [lodingState, setLoadingState] = useState(true);
   const musicList = useSelector((state) => state.musicList).data;
-  const result = [...musicList].sort((a,b)=>a.title < b.title? -1 :a.title>b.title?1:0)
+  const result = [...musicList].sort((a, b) =>
+    a.title < b.title ? -1 : a.title > b.title ? 1 : 0
+  );
   const [state, setState] = useState({ itemCount: 0, isLoading: false });
   const [open, setOpen] = useState(true);
   /* fake async fetch */
   const { itemCount, isLoading } = state;
 
   const fetchItems = async () => {
-      setState(prev => ({ ...prev, isLoading: true }));
-      await fakeFetch();
-      setState(prev => ({
-        itemCount: prev.itemCount + 13,
-        isLoading: false
-      }));
-      setLoadingState(false)
-    };
-    
+    setState((prev) => ({ ...prev, isLoading: true }));
+    await fakeFetch();
+    setState((prev) => ({
+      itemCount: prev.itemCount + 13,
+      isLoading: false,
+    }));
+    setLoadingState(false);
+  };
+
   useEffect(() => {
-      fetchItems();
+    fetchItems();
   }, []);
 
   const [_, setRef] = useIntersect(async (entry, observer) => {
-      observer.unobserve(entry.target);
-      await fetchItems();
-      observer.observe(entry.target);
+    observer.unobserve(entry.target);
+    await fetchItems();
+    observer.observe(entry.target);
   }, {});
 
   const handleClose = () => {
     setOpen(false);
   };
 
-  if(lodingState){
-    return(
+  if (lodingState) {
+    return (
       <>
         <SimpleBackdrop />
       </>
-    )
-  }else{
+    );
+  } else {
     return (
       <>
-      <div className="scrollEventContant">
-        <MainCardNav/>
-        <div className="scrollBox">
-        {[...Array(itemCount)].map((_, i) => {
-          if(result.length > i){
-            return <MainCard
-            id={i}
-            key={i}
-            music={result[i]}
-           />;
-          }else if(result.length +1 > i){
-            return <ListItem key={i} number={i} />;
-          }
-        })}
-          <div ref={setRef} className="Loading">
-            {isLoading && "Loading..."}
+        <div className="scrollEventContant">
+          {/* <MainCardNav/> */}
+          <div className="scrollBox">
+            {[...Array(itemCount)].map((_, i) => {
+              if (result.length > i) {
+                return <MainCard id={i} key={i} music={result[i]} />;
+              } else if (result.length + 1 > i) {
+                return <ListItem key={i} number={i} />;
+              }
+            })}
+            <div ref={setRef} className="Loading">
+              {isLoading && "Loading..."}
+            </div>
           </div>
-        </div>
         </div>
       </>
     );
