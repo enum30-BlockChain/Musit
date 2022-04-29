f060db676088e21fb5468ca188247f0d4f2180a4
limcuck <89626659+POcodingWER@users.noreply.github.com>
2022-04-29 14:07:14 +0900

    좋아요~

--- client/src/components/main/mypage/favoriteartist/FavoriteArtist.jsx
+++ client/src/components/main/mypage/favoriteartist/FavoriteArtist.jsx
@@ -5,6 +5,7 @@ import ArtistCard from "./ArtistCard";
 
 const FavoriteArtist = () => {
   const likeArtist = useSelector((state) => state.likeArtist);
+  console.log(likeArtist)
   return (
     <div
       className="favorite"
