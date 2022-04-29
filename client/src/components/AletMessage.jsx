21dc65af0ea75cbe0c338fcdd371d5e964df5d2a
limcuck <89626659+POcodingWER@users.noreply.github.com>
2022-04-29 17:44:01 +0900

    aletmessage 굿

--- client/src/components/AletMessage.jsx
+++ client/src/components/AletMessage.jsx
@@ -4,17 +4,17 @@ import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+
 const fakeFetch = (delay = 1500) =>
   new Promise((res) => setTimeout(res, delay));
 
-const Alert = forwardRef(function Alert(props, ref) {
+const Alert = forwardRef(function Alert(props, ref) {   //먼지잘모르겠음
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export default function Message(props) {
-  const music = useSelector((state) => state.music);
-  const [open, setOpen] = useState(false);
-  const [secondopen, setSecondOpen] = useState(false);
+export default function AletMessage(props) {
+  const [open, setOpen] = useState(true);
+  const [secondopen, setSecondOpen] = useState(true);
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -34,13 +34,12 @@ export default function Message(props) {
 
   useEffect(() => {
     const init = async () => {
-      if (props.onOff) {
+      if (props.onoff) {  //modal띄우게 해줄값
         if (music.errorMsg.length > 0) {
           handleClick();
         } else {
           handleClick2();
           await fakeFetch();
-          navigate("/artist/myupload", { state: props.title });
         }
       }
     };
@@ -53,9 +52,9 @@ export default function Message(props) {
         <Alert
           onClose={handleClose}
           severity="error"
-          sx={{ top: "35%", left: "43%", position: "fixed", width: 500 }}
+          sx={{ top: "2%", left: "34%", position: "fixed", width: 740 }}
         >
-          등록이 완료된 음원이거나 upload error가 발생하였습니다.
+        {props.message}
         </Alert>
       </Snackbar>
       <Snackbar
@@ -66,9 +65,9 @@ export default function Message(props) {
         <Alert
           onClose={handleClose}
           severity="success"
-          sx={{ top: "35%", left: "43%", position: "fixed", width: 500 }}
+          sx={{ top: "2%", left: "34%", position: "fixed", width: 740 }}
         >
-          음원이 정상등록되었습니다.
+          {props.message}
         </Alert>
       </Snackbar>
     </Stack>
