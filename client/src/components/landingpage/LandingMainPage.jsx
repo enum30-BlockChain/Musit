f82016d9ee84dd17e002dd31a9e2f8a8e1a5d7cd
imysh578 <33863016+imysh578@users.noreply.github.com>
2022-04-29 15:43:56 +0900

    edit end event  auction test && add landing page

--- client/src/components/landingpage/LandingMainPage.jsx
+++ client/src/components/landingpage/LandingMainPage.jsx
@@ -1,11 +1,12 @@
 import "./LandigMainPage.css";
 import React from "react";
+import Enummusic from "../main/music/enummusic/Enummusic";
 
 export default function LandingMainPage() {
   
   return (
     <>
-      
+      <Enummusic/>
     </>
   );
 }
